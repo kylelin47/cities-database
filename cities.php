ec1c1085a1beb0328bcbab3953e63c737d617a97
Joachim C. Jones <jojones@storm.cise.ufl.edu>
2014-11-03 18:00:56 -0500

    maps r us

--- cities.php
+++ cities.php
@@ -47,6 +47,31 @@ while ($row = oci_fetch_array($statement, OCI_ASSOC+OCI_RETURN_NULLS)) {
     foreach ($row as $item) {
         echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
     }
+    $latitude = $row['LATITUDE'];
+    //echo "  <td> LAT " . $latitude . "</td>\n";
+    $longitude = $row['LONGITUDE'];
+    //echo "  <td> LONG " . $longitude . "</td>\n";
+    if ($latitude < 0)
+    {
+            $NoS = 'S';
+            $latitude = abs($latitude);
+    } else {
+            $NoS = 'N';
+    }
+    $degrees = floor($latitude);
+    $minutes = ($latitude - $degrees) * 60;
+    $latitude = $degrees . '°' . $minutes . '\'' . $NoS;
+    if ($longitude < 0)
+    {
+            $WoE = 'W';
+            $longitude = abs($longitude);
+    } else {
+            $WoE = 'E';
+    }
+    $degrees = floor($longitude);
+    $minutes = ($longitude - $degrees) * 60;
+    $longitude = $degrees . '°' . $minutes . '\'' . $WoE;
+    echo "	<td><form action = 'http://google.com/maps/place/" . $latitude . "+" . $longitude . "><input type='submit' value = 'Map'></form></td>\n";
     echo "</tr>\n";
 }
 echo "</table>\n";
