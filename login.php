f5857a3d1c2c0b2fc8cacf02f894f6222b582f3e
qianwang1013 <qianwang1013@hotmail.com>
2014-11-03 18:36:24 -0500

    Update login.php

--- login.php
+++ login.php
@@ -13,5 +13,9 @@ $connection = oci_connect($username,
  	die("No Connection");
 
 
- 
+ //
+// VERY important to close Oracle Database Connections and free statements!
+//
+oci_free_statement($statement);
+oci_close($connection);
 ?>
