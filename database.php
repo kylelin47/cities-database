b9d627e077d5a2e83baa1b41d03c6241e5c9cf31
Kyle Lin <kylelin47@gmail.com>
2014-11-03 08:12:33 -0500

    Using Jinchi's testIndex. Opens in new tab.

--- database.php
+++ database.php
@@ -1,6 +1,5 @@
-#!/usr/local/bin/php
 <?php
 $username = 'kylin';
 $password = 'citiesdatabase';
-$connection_string = '//oracle.cise.ufl.edu/orcl');
+$connection_string = '//oracle.cise.ufl.edu/orcl';
 ?>
