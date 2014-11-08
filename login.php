d566c7b4dcdd692b8827097f8d878cdd00e13b96
qianwang1013 <qianwang1013@hotmail.com>
2014-11-07 22:59:35 -0500

    This should work now

--- login.php
+++ login.php
@@ -5,20 +5,41 @@ include "database.php";
  $User_password = $_POST['password'];
  
 if($User_username && $User_password ){
-	$connection = oci_connect($username,$password,$connection_string);
+        $connection = oci_connect('kylin','citiesdatabase','//oracle.cise.ufl.edu/orcl');
          
-         if($Connection){
+         if($connection){
+            $sql_User =oci_parse($connection, "SELECT * FROM login");
+            oci_execute($sql_User);
+        
+        $temp_result = FALSE;
+        while(($row = oci_fetch_assoc($sql_User))!= FALSE) {
+             if(($User_username == $row['USERNAME'])&&($User_password == $row['PASSCODE'])){
+                 $temp_result = TRUE;
+                 $FN = $row['FNAME'];
+                 $LA = $row['LNAME'];
+             }
+         }
+         if($temp_result == TRUE){
              echo '<html>';
+             echo "Welcom Back:"." &nbsp". $FN.",".$LA;
+             echo '<br>';
              echo '<a href = "index.html" >Click this Link back to homePage</a>';
              echo '</html>';
          }
-}
+         else{
+             echo '<html>';
+             echo '<a href = "User.html" >Invalid User and Password information</a>';
+             echo '</html>';
+         }
+         }
+         }
+
 else{
 	die('False input Or No input');
 }
  //
 // VERY important to close Oracle Database Connections and free statements!
 //
-oci_free_statement($statement);
+oci_free_statement($sql_User);
 oci_close($connection);
 ?>
