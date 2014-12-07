542229bf4db5492714afde31fecfaf41de70250b
Jinchi <jliu8023@gmail.com>
2014-12-07 15:55:33 -0500

    v2

--- admin.php
+++ admin.php
@@ -5,46 +5,8 @@ include "database.php";
 	$connection = oci_connect($username,
                               $password,
                               $connection_string);
-
-    echo "<!doctype html>";
-    echo "<html>";
-    echo "	<title>Admin</title>";
-    echo "	<link rel = 'stylesheet' href = 'styles/standard.css' type = 'text/css' />";
-    echo "	<body>";
-    echo "		<h2>Admin</h2>";
-    echo "		<div id = 'box'>";
-    echo "			<ul id = 'toolBar'>";
-    echo "				<li><a href = 'index.html'>Home</a></li>";
-    echo "				<li><a href = 'FAQ.html'>FAQ</a></li>";
-    echo "				<li><a href = 'Guide.html'>Guide</a></li>";
-    echo "				<li><a href = 'Queries.html'>Queries</a></li>";
-    echo "				<li><a href = 'User.php'>User</a></li>";
-    echo "			</ul>";
-    echo "<div class = 'userBox'>";
-    echo "You are " . $_SESSION['NAME'];
-    echo "<form id='logout' action='User.php' method='post'>";
-    echo "<input type='hidden' name='logout' value='1' />";
-    echo "<input type='submit' value='logout' />";
-	echo "</form>";
-	echo "<h1>possible actions:</h1>";
-
-    echo "<form>";
-    echo "Please select your action:";
-	echo "<select>";
-	echo "<option value='crtTbl'>create table</option>";
-	echo "<option value='insEle'>insert element</option>";
-	echo "<option value='rmTbl'>remove table</option>";
-	echo "<option value='rmEle'>remove element</option>";
-	echo "</select>";
-    echo "<input type='submit' value='".$adminAction."' />";
-    echo "</form>";
-    echo "</td>";
-    echo "</tr>";
-
-    echo "<a href='index.html'>Back Home</a>";
-    echo "</div>";
-    echo "		</div>";
-    echo "	</body>";
+							  
+	$adminAction = $_POST('action');
 	if ($adminAction = "crtTbl") {
 	echo "<form>Please enter the name of the table: <input type='text' value='".$tblName"'/><br>";
 	$statement = "CREATE TABLE ".$tblName." (";
